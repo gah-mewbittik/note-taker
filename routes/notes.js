09b19b50b5991aa17d5cbe4620ee233ec9cec338
Andrew Biron <andrewbiron@andrews-air.home>
2024-01-31 14:20:44 -0500

    adding GET to notes route file

--- routes/notes.js
+++ routes/notes.js
@@ -8,4 +8,6 @@ notes.get('/', (req, res) => {
     console.info(`${req.method} request received for notes`);
 
     readFromFile('/db/db.json').then ((data) => res.json(JSON.parse(data)));
-})
+});
+
+//Post
